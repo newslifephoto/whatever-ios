d63f8b38be76703ad045df4c8305e137d0da02b1
Ambas <ambaschobsanti@gmail.com>
2013-05-27 20:24:17 +0700

    create file interface
    controller and model interface

---
+++ LiveLocate 1/Classes/Controllers/ControllerInterface.h
@@ -0,0 +1,13 @@
+//
+//  ControllerInterface.h
+//  LiveLocate 1
+//
+//  Created by x5mobile on 5/27/56 BE.
+//  Copyright (c) 2556 LiveLocate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControllerInterface : UIApplication
+
+@end
