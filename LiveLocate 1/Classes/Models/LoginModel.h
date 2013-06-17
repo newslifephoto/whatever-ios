dc8767d957397a5acb86939506108412e1f9e2c5
Ambas <ambaschobsanti@gmail.com>
2013-06-17 18:32:35 +0700

    login model

---
+++ LiveLocate 1/Classes/Models/LoginModel.h
@@ -0,0 +1,13 @@
+//
+//  LoginModel.h
+//  LiveLocate 1
+//
+//  Created by x5mobile on 6/17/56 BE.
+//  Copyright (c) 2556 LiveLocate. All rights reserved.
+//
+
+#import "ModelInterface.h"
+
+@interface LoginModel : ModelInterface
+
+@end
---
+++ LiveLocate 1/Classes/Models/LoginModel.m
@@ -0,0 +1,13 @@
+//
+//  LoginModel.m
+//  LiveLocate 1
+//
+//  Created by x5mobile on 6/17/56 BE.
+//  Copyright (c) 2556 LiveLocate. All rights reserved.
+//
+
+#import "LoginModel.h"
+
+@implementation LoginModel
+
+@end
